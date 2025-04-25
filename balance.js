ecb8be31709ca51623824695ee9a469c2c7065a4
Ralphag-44 <ga2005br@outlook.com>
2025-04-25 20:04:23 -0300

    First commit

---
+++ balance.js
@@ -0,0 +1,83 @@
+class Balance
+{   constructor()
+    {   this.x = canvas.width/2;
+        this.radius = canvas.width/2;
+        this.y = canvas.height-this.radius;
+        this.angle = 0;
+        this.massCenter;
+        this.torqueTotal = 0;
+        this.points = [new Point(this.x-this.radius, this.y), new Point(this.x+this.radius, this.y)];
+    }
+    draw()
+    {   context.fillStyle = "Cyan";
+        context.beginPath();
+        context.arc(this.x, this.y, this.radius, 0+(Math.PI/180*this.angle), Math.PI+(Math.PI/180*this.angle), 0)
+        context.closePath();
+        context.fill();
+
+        context.fillStyle = "red";
+        context.beginPath();
+        context.arc(this.points[0].x, this.points[0].y, 5, 0, Math.PI*2, 0);
+        context.arc(this.points[1].x, this.points[1].y, 5, 0, Math.PI*2, 0);
+        context.closePath();
+        context.fill();
+        
+    }
+    update()
+    {   this.centerMass();
+        if(this?.massCenter)
+        {   let limitador = 2;
+            if(Math.abs(Math.trunc(this.angle)) == Math.abs(Math.trunc(this.torqueTotal*limitador)) && Math.sign(this.angle) == Math.sign(this.torqueTotal))
+            {   this.angle = this.torqueTotal*limitador;
+            }
+            else
+            {   if(Math.abs(this.angle) < Math.abs(this.torqueTotal*limitador))
+                {   this.angle += this.torqueTotal/20;
+                }
+                else
+                {   if(Math.sign(this.angle) == Math.sign(this.torqueTotal))
+                    {   this.angle -= this.torqueTotal/20;
+                    }
+                    else
+                    {   this.angle += this.torqueTotal/20;
+                    }
+                }
+            }
+            if(this.angle != this.points[0].angle)
+            {   let center = {x: this.x, y: this.y};
+                for(let i = 0; (i < this.points.length); i++)
+                {   this.points[i].translate(-center.x, -center.y);
+                    //this.points[i].rotate(this.angle+(this.points[i].angle*(i*2-1)));
+                    this.points[i].rotate(i==0 ? this.angle-this.points[0].angle : this.angle+this.points[0].angle)
+                    console.log(i*2-1, this.points[i].angle*(i*2-1), this.angle)
+                    this.points[i].translate(center.x, center.y);
+                }   
+            }
+        }
+        else
+        {   if(Math.trunc(this.angle) != 0)
+            {   this.angle += this.angle*-1/20;    
+            }
+            else
+            {   this.angle = 0;
+            }
+        }
+        if(Math.abs(this.angle) > 35)
+        {   window.location.reload();
+        }
+    }
+    centerMass() {
+        this.torqueTotal = 0;
+        for (let i = 0; i < pieces.length; i++) {
+            if(pieces[i].state)
+            {   let distance = Math.ceil((pieces[i].x + pieces[i].width/2) / (canvas.width / 20)) - 11;
+                this.torqueTotal += pieces[i].mass * gravity * distance;
+            }
+        }
+        this.massCenter = this.torqueTotal;
+    }
+
+
+    
+    
+}
---
+++ pieces.js
@@ -0,0 +1,87 @@
+let squareP;
+class Point
+{	constructor(x, y)
+	{	this.x = x;
+		this.y = y;
+        this.angle = 0;
+	}
+	translate(dx, dy)
+	{	this.x += dx;
+		this.y += dy;
+	}
+	rotate(angle)
+	{	this.angle += angle;
+        angle *= Math.PI/180;
+		let x = Math.cos(angle)*this.x-Math.sin(angle)*this.y;
+		let y = Math.sin(angle)*this.x+Math.cos(angle)*this.y;
+		this.x = x;
+		this.y = y;
+	}
+}
+
+
+class Piece
+{   constructor(type)
+    {   this.x = squareP*Math.trunc(Math.random()*19);
+        this.y = 0;
+        this.timer = 0;
+        this.state = true;
+        this.angle = 0;
+        switch(type)
+        {   case 0:
+                this.width = squareP*2;
+                this.height = squareP*2;
+                this.list = [new Point(this.x, this.y), new Point(this.x+this.width, this.y), new Point(this.x+this.width, this.y+this.height), new Point(this.x, this.y+this.height)];
+                this.color = "yellow";
+                this.mass = 4;
+            break;
+        }
+    }
+    translate(dx, dy)
+    {   for(let i = 0; (i < this.list.length); i++)
+        {   this.list[i].translate(dx, dy);
+        }
+        this.x+=dx;
+        this.y+=dy;
+    }
+    update()
+    {   if(!this.state)
+        {   this.timer++;
+            if(this.timer == 30)
+            {   this.translate(0, squareP);
+                this.timer = 0;
+            }
+        }
+        if(this.angle != balance.angle)
+        {   this.rotate(balance.angle-this.angle);
+            this.angle = balance.angle;
+        }
+    }
+    draw()
+    {   context.fillStyle = this.color;
+        context.beginPath();
+        context.moveTo(this.list[0].x, this.list[0].y);
+        for(let i = 0; (i < this.list.length); i++)
+        {   context.lineTo(this.list[i].x, this.list[i].y);
+        }
+        context.closePath();
+        context.fill();
+    }
+    rotate(angle)
+    {   let center = {x: this.x+this.width/2, y: this.y+this.height/2};
+        this.translate(-center.x, -center.y);
+        for(let i = 0; (i < this.list.length); i++)
+        {   this.list[i].rotate(angle);
+        }
+        this.angle += angle;
+        this.translate(center.x, center.y);
+    }
+
+
+    
+
+
+
+
+
+}
